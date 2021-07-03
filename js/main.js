dcc71304d898ec5ed0757d3735cc76730d8baa75
tiagosquall <86284163+tiagosquall@users.noreply.github.com>
2021-07-03 04:13:17 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,67 @@
+
+
+
+
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+
+/*
+var count;
+for(count= 1; count <= 5; count++){
+    alert(count);
+};
+*/
+
+
+/*
+var count =  0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt("Qual a sua idade?")
+if (idade >= 18){
+    alert("maior de idade");    
+}else{
+    alert("menor de idade");
+};
+*/
+
+
+
+
+
+//var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+//console.log(frutas);
+//alert(frutas[1].nome);
+
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["Maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista.length);
+//console.log(lista.join(" - "))
+
+//var nome = "Tiago Cruz"; 
+//var idade = 32;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem "+ idade + " Anos");
+//alert(idade + idade2)
+//onsole.log(nome);
+//console.log(idade + idade2); 
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"))
